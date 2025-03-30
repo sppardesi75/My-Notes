b83db26e3adb780dfc56e9fce70f5f8aad8c87fb
Sanskar Pardesi <sanskarpardesi@gmail.com>
2025-03-30 18:49:18 -0400

    Initial Commit

---
+++ MyNotes/MyNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNotesApp.swift
+//  MyNotes
+//
+//  Created by Sanskar Pardesi on 2025-03-30.
+//
+
+import SwiftUI
+
+@main
+struct MyNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
